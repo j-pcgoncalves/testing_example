3700e547b9f23a52623af66c9b032337924fd83a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-22 22:26:49 +0000

    write async tests

--- script_2.test.js
+++ script_2.test.js
@@ -1,1 +1,18 @@
 const swapi = require("./script_2");
+
+it("calls swapi to get people", (done) => {
+    expect.assertions(1);
+    swapi.getPeople().then(data => {
+        expect(data.count).toEqual(87);
+        done();
+    })
+});
+
+it("calls swapi to get people with a promise", (done) => {
+    expect.assertions(2);
+    swapi.getPeoplePromise().then(data => {
+        expect(data.count).toEqual(87);
+        expect(data.results.length).toBeGreaterThan(5);
+        done();
+    })
+});
